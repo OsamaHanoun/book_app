01a4b4f1fcf6bd0eb5b6c91d82464e73cca4e264
Osama Hanoun <osamahanoun997@gmail.com>
2020-07-12 14:58:00 +0300

    init and proof of life

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+require('dotenv').config();
+
+const express = require('express');
+const superagent = require('superagent');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+
+app.get('/hello',(req,res)=>{
+    res.render('pages/index');
+});
+app.get('/searches/new',(req,res)=>{
+    res.render('pages/searches/new');
+});
+
+app.get('*',(req,res)=>{
+    res.status(404).send(`This route doesn't exist`);
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`);
+})
